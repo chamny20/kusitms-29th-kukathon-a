0c0a380c9ec49cbb900f18f6f6c9a4ab4ef04e2b
chamny20 <chamny20@naver.com>
2024-04-06 21:28:44 +0900

    test: test kakao-login (#1)

---
+++ src/pages/test/TestPage.tsx
@@ -0,0 +1,9 @@
+import KakaoLogin from '../../components/login/KakaoLogin';
+
+export const TestPage = () => {
+  return (
+    <div style={{ width: '273px' }}>
+      <KakaoLogin />
+    </div>
+  );
+};

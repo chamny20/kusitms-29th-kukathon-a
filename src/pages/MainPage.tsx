c7a18d069cc3bc3125f78218067ebeffc06eff5e
yyypearl <hor629@naver.com>
2024-04-06 16:46:14 +0900

    test: test toast

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,9 +1,12 @@
+import { notify } from '../hooks/notify';
+
 export const MainPage = () => {
   return (
     <>
       hello world
       <h2>안녕하세요</h2>
       <div style={{ fontWeight: 'bold' }}>hihiihihi</div>
+      <button onClick={() => notify({ text: 'hi', icon: '🤍' })}>Toast</button>
     </>
   );
 };
